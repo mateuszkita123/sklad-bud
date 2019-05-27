611e3d3f120701a05386a87e720d2eea70154848
bimmateusz <mateuszkita123@gmail.com>
2019-05-27 17:18:30 +0200

    fixed while loop in store ts

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -24,35 +24,34 @@ export class StoreComponent implements OnInit {
     this.message = "Odebrano partię " + amount + " cegieł w cenie " + price + " PLN/szt.";
   }
 
-  pull(amount: number): void {
-    if(!amount || !this.storeArr.length) { return; };
+  pull(sellAmount: number): void {
+    if(!sellAmount || !this.storeArr.length) { return; };
     let sum: number = 0;
+    let bricksAmount: number = +sellAmount;
     for(let i=0; i<this.storeArr.length; i++) {
       sum += +this.storeArr[i].amount;
     };
-    if(amount > sum) {
+    if(sellAmount > sum) {
       this.message = "Podana ilość cegieł jest niewystarczająca do zrealizowania zamówienia! Dodaj kolejną dostawę cegieł.";
     } else {
-        let transactionValue: number = 0;
-        let bricksAmount: number = amount;
-        while (bricksAmount >= 0) {
-          let i: number = 0;
-          if(bricksAmount < this.storeArr[i].amount) {
-              this.storeArr[i].amount -= bricksAmount;
-              transactionValue += (bricksAmount * this.storeArr[i].price);
-              this.message = "Wartość zakupu cegieł wyniosła: " + transactionValue + " PLN.";
-              break;
-          } else if(bricksAmount == this.storeArr[i].amount) {
-              transactionValue += (this.storeArr[i].amount * this.storeArr[i].price);
-              this.message = "Wartość zakupu cegieł wyniosła: " + transactionValue + " PLN.";
-              this.storeArr.splice(0, 1);
-              break;
-          } else if(bricksAmount > this.storeArr[i].amount) {
-              transactionValue += (this.storeArr[i].amount * this.storeArr[i].price);
-              bricksAmount -= this.storeArr[i].amount;
-              this.storeArr.splice(0, 1);
-          } else { return; }
-        }
+      let transactionValue: number = 0;
+      while (+bricksAmount >= 0) {
+        let i: number = 0;
+        if(bricksAmount < +this.storeArr[i].amount) {
+            this.storeArr[i].amount -= bricksAmount;
+            transactionValue += (bricksAmount * +this.storeArr[i].price);
+            break;
+        } else if(bricksAmount == +this.storeArr[i].amount) {
+            transactionValue += (+this.storeArr[i].amount * +this.storeArr[i].price);
+            this.storeArr.splice(0, 1);
+            break;
+        } else if(bricksAmount > +this.storeArr[i].amount) {
+            transactionValue += (+this.storeArr[i].amount * +this.storeArr[i].price);
+            bricksAmount -= +this.storeArr[i].amount;
+            this.storeArr.splice(0, 1);
+        } else { return; }
+      }
+      this.message = "Wartość zakupu cegieł wyniosła: " + transactionValue + " PLN.";
     }
   }
 }
